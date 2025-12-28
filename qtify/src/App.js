2d7b8219dfccd4c9389b4a552333e5dce3f51524
Madhumathi Raman <hariniraman28@gmail.com>
2025-12-28 22:07:11 +0530

    Updated App.js

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
-import Section from './components/Section/Section';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar searchData={[]} />
       <Hero />
-      {/* Add sections for Top Albums, New Albums, Songs, etc. */}
     </div>
   );
 }
