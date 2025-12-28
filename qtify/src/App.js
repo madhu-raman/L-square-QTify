305545b20e1ab5fed0d651164449d96c5684235f
Madhumathi Raman <hariniraman28@gmail.com>
2025-12-28 22:49:54 +0530

    Wrap app in Router to fix routing context

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,15 +1,18 @@
-import './App.css';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 
 function App() {
-    return (
-        <div className="App">
-          <Navbar searchData={[]} />
-          <Hero />
-        </div>
-      );
+  return (
+    <Router>
+      <div className="App">
+        <Navbar searchData={[]} />
+        <Hero />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
