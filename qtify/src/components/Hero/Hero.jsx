0af2ba09b68e8d9dbe9699c1353e255ec2c1b3b6
Madhumathi Raman <hariniraman28@gmail.com>
2025-12-29 09:32:33 +0530

    Updated Hero Image

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Hero.module.css";
-import HeroImage from "../../assets/vibrating-headphone.png";
+import HeroImage from "../../assets/hero_headphone.png";
 
 function Hero() {
   return (
